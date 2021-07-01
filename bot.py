fe23c4473070ad3613da24397d335f56e5304438
Coding Hub <76939047+reality-studio@users.noreply.github.com>
2021-07-01 13:49:40 +0000

    Added token

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
-import os
+import os 
+import discord
+from discord.ext import commands
 from Website.flask import keep_alive as start
 
-start()
+bot = commands.Bot(
+  command_prefix="!"
+  )
+  
+if __name__ == "__bot__":
+  token = os.environ["token"]
+  start()
+  bot.run()
