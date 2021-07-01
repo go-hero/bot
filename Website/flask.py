74e3dc162547e3ff6eccd3e8f8635eaf5edbd900
Coding Hub <76939047+reality-studio@users.noreply.github.com>
2021-07-01 14:23:02 +0000

    Created https://www.gohero.cf/invite

--- Website/flask.py
+++ Website/flask.py
@@ -12,9 +12,12 @@ def bot():
   return render_template(
     "bot.html",
     home=url_for("home"),
-    url=url_for('static', filename="text.png")
     )
-  
+
+@app.route('/invite')
+def invite():
+  return redirect("https://discord.com/api/oauth2/authorize?client_id=857634436584570920&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FpwMQ3wrf3e&response_type=code&scope=bot%20applications.commands%20guilds.join")
+
 def run():
   app.run(host="0.0.0.0",port=8080)
   
--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@ import discord
 from discord.ext import commands
 from Website.flask import keep_alive as start
 
+import cogs
+
 bot = commands.Bot(
   command_prefix="!"
   )
   
-if __name__ == "__bot__":
-  token = os.environ["token"]
-  start()
-  bot.run()
+token = os.environ["token"]
+start()
+bot.run(token)
