1ce14854bb5cf94bb4111c758b3dba4801cf0edc
Coding Hub <76939047+reality-studio@users.noreply.github.com>
2021-07-01 07:15:06 +0000

    Updated website

--- Website/flask.py
+++ Website/flask.py
@@ -11,14 +11,8 @@ def home():
 def bot():
   return render_template(
     "bot.html",
-    home=url_for("home"),
-    text = url_for("logo")
+    home=url_for("home")
     )
-
-#sources
-@app.route('/admin/source/text.jpg')
-def logo():
-  return render_template("20210701_122531.png")
   
 def run():
   app.run(host="0.0.0.0",port=8080)
