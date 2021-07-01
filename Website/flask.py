bc76c318353977e4f39a6f13b223a1cd97f50e88
Coding Hub <76939047+reality-studio@users.noreply.github.com>
2021-07-01 06:46:22 +0000

    Changes on website

--- Website/flask.py
+++ Website/flask.py
@@ -9,8 +9,16 @@ def home():
   
 @app.route('/bot')
 def bot():
-  return render_template("bot.html", home=url_for("home"))
- 
+  return render_template(
+    "bot.html",
+    home=url_for("home"),
+    text = url_for("logo")
+    )
+
+@app.route('/admin/source/text.jpg')
+def logo():
+  return render_template("text.jpg")
+  
 def run():
   app.run(host="0.0.0.0",port=8080)
   
